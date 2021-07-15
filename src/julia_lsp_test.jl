c63af51949ff15bac6d82b46c7be65e7824982f8
Dheepak Krishnamurthy <me@kdheepak.com>
2021-07-15 14:05:39 -0600

    Update files

--- src/julia_lsp_test.jl
+++ src/julia_lsp_test.jl
@@ -3,6 +3,13 @@ module julia_lsp_test
 using JSON
 using JSON3
 
+using PackageCompiler
+using Franklin
+
 greet() = print("Hello World!")
+
+function hello()
+    println("hello world")
+end
 
 end # module
